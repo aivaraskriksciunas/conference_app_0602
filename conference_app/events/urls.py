1e799c4c5a21e61ed6594f14e4bf90e690c3eb38
aivaraskriksciunas <aiviskri@gmail.com>
2023-06-05 19:00:40 +0300

    Update visitor count

--- conference_app/events/urls.py
+++ conference_app/events/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from events.views import EventDetailView
+from events.views import (
+    EventDetailView,
+    register_visitor
+)
 
 urlpatterns = [
     # /events/2/
-    path( '<int:pk>/', EventDetailView.as_view() )
+    path( '<int:pk>/', EventDetailView.as_view() ),
+    path( 'register/<int:renginio_id>/', register_visitor ),
 ]
--- conference_app/events/views.py
+++ conference_app/events/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
 from events.models import Event
 
 class EventDetailView( DetailView ):
     model = Event
     context_object_name = "renginys"
+
+# View, kurį pasiekus prie lankytojų skaičiaus bus pridėtas 1
+def register_visitor( request, renginio_id ):
+    event = get_object_or_404( Event, id = renginio_id )
+    event.visitors += 1
+    event.save() # UPDATE events SET visitors = 2 WHERE id = 1
+
+    return HttpResponse( event.title )

00a51b86030a16e18743877ce3e06ad906fbfdc8
aivaraskriksciunas <aiviskri@gmail.com>
2023-06-13 19:13:29 +0300

    Konferencijų komentarai

--- conference_app/conferences/urls.py
+++ conference_app/conferences/urls.py
@@ -1,6 +1,6 @@
 from .views import (
     ConferenceListView,
-    ConferenceDetailView
+    ConferenceDetailView, CreateCommentView
 )
 from django.urls import path
 
@@ -8,5 +8,6 @@ urlpatterns = [
     # Visi views kurie yra ne funkcijos, o klases,
     # juos kvieciam su papildoma as_view()
     path( "", ConferenceListView.as_view() ), # /conferences/
-    path( "<int:pk>/", ConferenceDetailView.as_view(), name = "conference-detail" ) # /conferences/1/, /conferences/10/
+    path( "<int:pk>/", ConferenceDetailView.as_view(), name = "conference-detail" ), # /conferences/1/, /conferences/10/
+    path( "<int:konferencijos_id>/comments/", CreateCommentView.as_view(), name='create-comment' ),
 ]
--- conference_app/conferences/views.py
+++ conference_app/conferences/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views import View
 from django.views.generic import ListView, DetailView
-from .models import Conference
+from .models import Conference, Comment
 
 
 # 	Sukurkite ListView, kuris atvaizduotų visas konferencijas
@@ -15,7 +18,34 @@ class ConferenceListView( ListView ):
 class ConferenceDetailView( DetailView ):
     model = Conference
 
+
 # Alternatyva DetailView
 # def conference_detail( request, pk ): # pk ateina iš urls failo: path( "<int:pk>/", ... )
 #     conference = get_object_or_404( Conference, pk = pk )
 #     return render( request, "conferences/conference_detail.html", { "object": conference } )
+
+class CreateCommentView( View, LoginRequiredMixin ):
+    # konferencijos_id -> turi sutapti su pavadinimu urls.py faile!
+    def post( self, request, konferencijos_id ):
+        # 0. išsirašyti visas reikšmes
+        komentaro_tekstas = request.POST.get( 'komentaras' )
+        # 1. Validacija
+        if len( komentaro_tekstas ) == 0:
+            return HttpResponse( "Klaida! negali būti tuščias" )
+        # tikrinam ar vartotojas prisijungęs
+        # Tikrinimo nereikia jei naudojam LoginRequiredMixin
+        # if request.user.is_authenticated == False:
+        #     return HttpResponse( "Klaida! Jums reikia prisijungti" )
+
+        # Vienu metu patikriname, ar yra konferencija su konferencijos_id, ir
+        # taip pat išsaugome Conference objektą su tuo konferencijos_id
+        konferencija = get_object_or_404( Conference, id=konferencijos_id )
+
+        # 2. Veiksmas - įrašyti komentarą į duomenų bazę
+        komentaras = Comment()
+        komentaras.author = request.user
+        komentaras.conference = konferencija
+        komentaras.comment = komentaro_tekstas
+        komentaras.save()
+        # 3. Rezultatas vartotojui
+        return redirect( f"/conferences/{konferencijos_id}" )
